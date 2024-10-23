50c3f2b26aa0c2057ca0482258a98104c4e21ee2
IDS-Mandujano <jesusgutierrez191105@gmail.com>
2024-10-23 10:53:13 -0600

    Add formSubasta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,16 @@ import { Home } from "./home";
 import Login from "./Login/Login";
 import { Ofertas } from "./Ofertas/";
 import Registro from "./Registro/Registro";
+import path from "path";
+import { Subastalo } from "./Subastalo";
 
 const routes = [
   { path: "/", Page: Registro }, 
   { path: "/home", Page: Home },
   { path: "/ofertas", Page: Ofertas },
   { path: "/login", Page: Login },
-  { path: "/usuario", Page: Login } 
+  { path: "/usuario", Page: Login },
+  {path: "/subastalo", Page:Subastalo}
 ];
 
 function Routing() {
@@ -18,7 +21,7 @@ function Routing() {
   return (
     <Routes location={location} key={location.pathname}>
       {routes.map(({ path, Page }) => (
-        <Route key={path} path={path} element={<Page />} /> // Cambiar <Routes /> a <Page />
+        <Route key={path} path={path} element={<Page />} /> 
       ))}
     </Routes>
   );
